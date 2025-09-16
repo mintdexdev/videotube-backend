fb74ae1ac653e48a044f92908e98500591726f60
mintdexdev <dexdevpersona@gmail.com>
2025-09-16 07:00:00 +0530

    add video  model

---
+++ src/models/video.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from 'mongoose';
+
+const videoSchema = new Schema({
+  videoFile: {
+    type: String, // cloudnary url
+    required: true,
+  },
+  thumbnail: {
+    type: String, // cloudnary url
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    re: "User"
+  }
+
+}, { timestamps: true })
+
+export const Video = mongoose.model("Video", videoSchema);
