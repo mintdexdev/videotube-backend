8076309a152367cf79b91b807d077afab3631be1
mintdexdev <dexdevpersona@gmail.com>
2025-09-17 06:00:00 +0530

    add jwt and encryption

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
   username: {
@@ -16,7 +18,7 @@ const userSchema = new Schema({
     lowercase: true,
     trim: true,
   },
-  fullname: {
+  fullName: {
     type: String,
     required: [true, "Full Name is required"],
     trim: true,
@@ -44,5 +46,37 @@ const userSchema = new Schema({
     type: String,
   }
 }, { timestamps: true })
+
+userSchema.pre("saave", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10)
+  next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    { _id: this._id },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPITY }
+  )
+}
 
 export const User = mongoose.model("User", userSchema) 
